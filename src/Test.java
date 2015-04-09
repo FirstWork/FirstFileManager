0ad0c778947f5de960a4fc21e19012955e831eac
NikolayVasiletskij <nikolay.vasileckij@gmail.com>
2015-04-10 00:50:48 +0300

    Добавил layout manager'ов немного

--- src/Test.java
+++ src/Test.java
@@ -28,11 +28,17 @@ public class Test {
 
         JFrame frame = new JFrame();
 
-        JButton goToRoot = new JButton("GoToRoot");
-        goToRoot.setMinimumSize(new Dimension(10, 20));
+        JButton goToRootLeft = new JButton("GoToRoot");
+        goToRootLeft.setPreferredSize(new Dimension(80, 20));
 
-        JButton goToParent = new JButton("GoToParent");
-        goToParent.setMaximumSize(new Dimension(10,20));
+        JButton goToParentLeft = new JButton("GoToParent");
+        goToParentLeft.setPreferredSize(new Dimension(80, 20));
+
+        JButton goToRootRight = new JButton("GoToRoot");
+        goToRootRight.setPreferredSize(new Dimension(80, 20));
+
+        JButton goToParentRight = new JButton("GoToParent");
+        goToParentRight.setPreferredSize(new Dimension(80,20));
 
         JPanel windowLeft = new JPanel();
         windowLeft.setMaximumSize(new Dimension(150, 200));
@@ -43,15 +49,61 @@ public class Test {
         windowRight.setMinimumSize(new Dimension(150, 200));
         windowRight.add(new Button("Правое окно файлового менеджера") , BorderLayout.WEST);
 
+        JTextField jLabelLeft = new JTextField("Введите сюда путь к папке для окна слева");
+        JTextField jLabelRight = new JTextField("Введите сюда путь к папке для окна справа");
+
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setMinimumSize(new Dimension(600, 400));
-        frame.setLayout(new GridLayout(3, 2, 50, 30));
-        frame.add(goToRoot);
-        frame.add(goToParent);
-        frame.add(new JLabel("Введите сюда путь к папке для окна слева"));
-        frame.add(new JLabel("Введите сюда путь к папке для окна справа"));
-        frame.add(windowLeft, BorderLayout.WEST);
-        frame.add(windowRight, BorderLayout.EAST);
+
+
+        GridBagLayout gridBagLayout = new GridBagLayout();
+        frame.setLayout(gridBagLayout);
+
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+
+        gridBagConstraints.anchor = GridBagConstraints.CENTER;
+        gridBagConstraints.fill   = GridBagConstraints.NONE;
+        gridBagConstraints.gridheight = 1;
+        gridBagConstraints.gridwidth  = 1;
+        gridBagConstraints.gridx = GridBagConstraints.RELATIVE;
+        gridBagConstraints.gridy = GridBagConstraints.RELATIVE;
+        gridBagConstraints.insets = new Insets(10, 10, 10, 10);
+        gridBagConstraints.ipadx = 0;
+        gridBagConstraints.ipady = 0;
+        gridBagConstraints.weightx = 0.0;
+        gridBagConstraints.weighty = 0.0;
+
+        gridBagLayout.setConstraints(goToParentLeft, gridBagConstraints);
+
+        gridBagLayout.setConstraints(goToRootLeft, gridBagConstraints);
+
+        gridBagLayout.setConstraints(goToParentRight, gridBagConstraints);
+
+        gridBagConstraints.gridwidth = GridBagConstraints.REMAINDER;
+
+        gridBagLayout.setConstraints(goToRootRight, gridBagConstraints);
+
+        gridBagConstraints.gridwidth = 1;
+        gridBagLayout.setConstraints(jLabelLeft, gridBagConstraints);
+        gridBagConstraints.gridwidth = GridBagConstraints.REMAINDER;
+        gridBagLayout.setConstraints(jLabelRight, gridBagConstraints);
+
+        gridBagConstraints.gridwidth = 1;
+        gridBagLayout.setConstraints(windowLeft, gridBagConstraints);
+        gridBagConstraints.gridwidth = GridBagConstraints.REMAINDER;
+        gridBagLayout.setConstraints(windowRight, gridBagConstraints);
+
+
+        //frame.setLayout(new GridLayout(3, 2, 50, 30));
+
+        frame.add(goToParentLeft);
+        frame.add(goToRootLeft);
+        frame.add(goToParentRight);
+        frame.add(goToRootRight);
+        frame.add(jLabelLeft);
+        frame.add(jLabelRight);
+        frame.add(windowLeft);
+        frame.add(windowRight);
         frame.pack();
         frame.setVisible(true);
 
